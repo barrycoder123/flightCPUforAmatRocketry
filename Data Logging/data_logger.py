3f97071e4bca6656ab978d600f1b45876ae6df3c
Zack Rummler <zrummler@Zacks-MacBook-Pro.local>
2023-03-31 13:14:37 -0400

    removed plotting

--- Data Logging/data_logger.py
+++ Data Logging/data_logger.py
@@ -10,7 +10,7 @@ If num_points is None, then
 import time
 import numpy as np
 #import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 class DataLogger:
         
@@ -113,62 +113,62 @@ class DataLogger:
         
         
      
-    def __plotData(self, x, t, cov=None, name='Position', unit='m', subx0=False, decim_fact=1, tEnd=None, axes=None):
-        """
-        TODO
+    # def __plotData(self, x, t, cov=None, name='Position', unit='m', subx0=False, decim_fact=1, tEnd=None, axes=None):
+    #     """
+    #     TODO
 
-        Parameters
-        ----------
-        x : (M,N) ndarray
-            matrix of measured values, where ``x[:,i]`` is the vector of measured quantities at time `i`
-        x_true : (M,N) ndarray
-            matrix of truth values, where ``x_true[:,i]`` is the vector of true values at time `i`. Same shape as ``x``
-        t : (N,) ndarray
-            time vector [seconds]
-        cov : (M, M, N) ndarray
-            if provided, specifies the covariance for the data. This will be plotted in the error graphs
-        name : str, default='Position'
-            plot name to appear in titles and axis labels
-        unit : str, default='m'
-            unit string for axis labels
-        subx0 : bool, default=False
-            if True, the true value at the first time will be subtracted from both vectors. This is useful when plotting quantities with a large offset,
-            such as ECEF position
-        decim_fact : int, default=1
-            decimation factor for plotting
-        tEnd : float
-            if provided, the xlim will be set to end at this time
-        axes : list
-            axis names. Default is ['x', 'y', 'z']. Length match x.shape[0]
-        """
+    #     Parameters
+    #     ----------
+    #     x : (M,N) ndarray
+    #         matrix of measured values, where ``x[:,i]`` is the vector of measured quantities at time `i`
+    #     x_true : (M,N) ndarray
+    #         matrix of truth values, where ``x_true[:,i]`` is the vector of true values at time `i`. Same shape as ``x``
+    #     t : (N,) ndarray
+    #         time vector [seconds]
+    #     cov : (M, M, N) ndarray
+    #         if provided, specifies the covariance for the data. This will be plotted in the error graphs
+    #     name : str, default='Position'
+    #         plot name to appear in titles and axis labels
+    #     unit : str, default='m'
+    #         unit string for axis labels
+    #     subx0 : bool, default=False
+    #         if True, the true value at the first time will be subtracted from both vectors. This is useful when plotting quantities with a large offset,
+    #         such as ECEF position
+    #     decim_fact : int, default=1
+    #         decimation factor for plotting
+    #     tEnd : float
+    #         if provided, the xlim will be set to end at this time
+    #     axes : list
+    #         axis names. Default is ['x', 'y', 'z']. Length match x.shape[0]
+    #     """
         
         
-        if axes is None:
-            axes = ['X', 'Y', 'Z']
+    #     if axes is None:
+    #         axes = ['X', 'Y', 'Z']
 
-        if len(axes) != x.shape[0]:
-            raise ValueError('len(axes) != x.shape[0]')
+    #     if len(axes) != x.shape[0]:
+    #         raise ValueError('len(axes) != x.shape[0]')
 
-        x0_use = x[:, 0] if subx0 else np.zeros((len(axes),))
+    #     x0_use = x[:, 0] if subx0 else np.zeros((len(axes),))
 
-        fig, axs = plt.subplots(len(axes), 1, sharex=True, figsize=(13, 8))
-        plt.suptitle(name, fontsize='xx-large', fontweight='black')
+    #     fig, axs = plt.subplots(len(axes), 1, sharex=True, figsize=(13, 8))
+    #     plt.suptitle(name, fontsize='xx-large', fontweight='black')
 
-        tplot = t[::decim_fact]
-        for i, row in enumerate(axs):
-            data = x[i, ::decim_fact]
-            row.plot(tplot, data - x0_use[i])
-            row.set_xlabel('Time [sec]')
-            row.set_ylabel(f'[{unit}]')
-            row.set_title(f"{axes[i]}")
-            row.grid(True)
+    #     tplot = t[::decim_fact]
+    #     for i, row in enumerate(axs):
+    #         data = x[i, ::decim_fact]
+    #         row.plot(tplot, data - x0_use[i])
+    #         row.set_xlabel('Time [sec]')
+    #         row.set_ylabel(f'[{unit}]')
+    #         row.set_title(f"{axes[i]}")
+    #         row.grid(True)
 
 
-            if tEnd is not None:
-                row.set_xlim([0, tEnd])
+    #         if tEnd is not None:
+    #             row.set_xlim([0, tEnd])
 
-        plt.tight_layout()
-        return fig, axs
+    #     plt.tight_layout()
+    #     return fig, axs
         
     
     
