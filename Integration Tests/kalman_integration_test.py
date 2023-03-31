70893e6be307aa33e35f8752bae64481feed6882
Zack Rummler <zrummler@Zacks-MacBook-Pro.local>
2023-03-31 12:00:43 -0400

    added GPS warmup

--- Integration Tests/kalman_integration_test.py
+++ Integration Tests/kalman_integration_test.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     collector = dc.DataCollector().create()
     num_points = collector.num_points
     
+    # Warm up GPS
     satellites = 0
     while satellites < 8:
         lla, satellites = collector.get_next_gps_reading()
