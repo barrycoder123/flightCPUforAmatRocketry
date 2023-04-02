28284b022073ab4a101dc788d765537471a0e1ef
Zack Rummler <zrummler@Zacks-MacBook-Pro.local>
2023-04-02 17:35:28 -0400

    added print statement

--- Data Collection/gps_code_modified.py
+++ Data Collection/gps_code_modified.py
@@ -105,14 +105,14 @@ if __name__ == "__main__":
     timestamp = time.monotonic()
     while True:
         data = gps.readline()  # read up to 32 bytes
+        
+        print("READ A LINE!!")
         # print(data)  # this is a bytearray type
 
         if data is not None:
             # convert bytearray to string
             data_string = "".join([chr(b) for b in data])
             print(data_string, end="")
-        else:
-            print(None)
 
         # if time.monotonic() - timestamp > 5:
         #     # every 5 seconds...
